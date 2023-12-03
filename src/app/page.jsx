39fe7dc50d4ba5a65f07cd085ecefad796d8005f
Dicky <dickyferdy5@gmail.com>
2023-12-03 11:54:04 +0700

    add loading effect & toTop button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,34 @@
+"use client";
+
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/header";
 import getAnimeResponse from "./service/api-service";
+import { useEffect, useState } from "react";
+import ToTop from "@/components/Utilities/ToTop";
 
-const Page = async () => {
+const Page = () => {
 
-  const topAnime = await getAnimeResponse('top/anime', 'limit=10');
+  const [topAnime, setTopAnime] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => { 
+    const data = await getAnimeResponse('top/anime', `limit=10`);
+    setTopAnime(data);
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <section>
         <Header title="Paling Populer" linkHref="/populer" linkTitle="Lihat Semua" />
         <AnimeList api={topAnime} />
+        {loading && 
+          <ToTop />
+        }
       </section>
     </>   
   );
