bd8df72ec125d07c9df46abe65a1d649b2157d5f
Dicky <dickyferdy5@gmail.com>
2023-12-06 09:52:53 +0700

    add UserActionButton & styling

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import InputSearch from "./inputSearch";
+import UserActionButton from "./userActionButton";
 
 const Navbar = () => {
   return (
     <header className="bg-color-accent">
-      <div className="flex md:flex-row flex-col md:items-center justify-between p-4 gap-2">
-        <Link href='/' className="font-bold text-3xl" >ANIMELIST</Link>
+      <div className="flex md:flex-row flex-col md:items-center justify-between p-3 gap-2">
+        <Link href='/' className="font-bold text-3xl" >ANIMEPEDIA</Link>
         <InputSearch />
+        <UserActionButton />
       </div>
     </header>
   );
