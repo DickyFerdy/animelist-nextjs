96e24921b60f70efc1b4baac63409edba8f5434c
Dicky <dickyferdy5@gmail.com>
2023-12-08 19:14:05 +0700

    create AnimeListRecom

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import CardCarousel from "../Recommend";
 
-const AnimeList = ({ api }) => {
+export const AnimeList = ({ api }) => {
   return (
     <div className="grid md:grid-cols-6 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {api.data?.map((anime, index) => {
@@ -15,4 +16,19 @@ const AnimeList = ({ api }) => {
   );
 };
 
-export default AnimeList;
+export const AnimeListRecom = ({ api }) => {
+  return (
+    <div className="mx-4 px-3 text-center">
+      <CardCarousel card=
+        {api.data?.map((anime, index) => {
+          return (
+            <Link href={`/anime/${anime.mal_id}`} key={index} className="cursor-pointer text-color-primary hover:text-color-accent transition-all">
+              <img className="w-full max-h-80 object-cover px-3" src={anime.images.webp.image_url} alt="..."/>
+              <h3 className="font-bold p-4">{anime.title}</h3>
+            </Link>
+          );
+        })}
+      />
+    </div>
+  )
+}
