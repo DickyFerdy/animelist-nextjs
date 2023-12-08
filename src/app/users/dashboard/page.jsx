1e8a26aac1ef64008a42e4373c6121fc8759d302
Dicky <dickyferdy5@gmail.com>
2023-12-08 19:28:27 +0700

    dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,24 +1,12 @@
 import { authUserSession } from "@/service/auth-service"
-import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
 
   return (
-    <>
-      <div className="text-color-primary">
-        
-        {user ? <h1>{user?.name}</h1> : 
-          <div className="flex justify-center items-center h-96">
-            <div className="flex justify-center items-center gap-2 flex-col">
-              <h1 className="text-color-primary text-2xl">Please sign in to view the dashboard</h1>
-              <Link href={'/'}>
-                <button className="bg-color-accent text-color-dark rounded p-2">Home</button>
-              </Link>
-            </div>
-          </div>}
-      </div>
-    </>
+    <div className="text-color-primary">
+      <h1>Welcome {user?.name}</h1>
+    </div>
   );
 };
 
