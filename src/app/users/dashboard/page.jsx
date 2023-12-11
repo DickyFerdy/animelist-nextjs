c773cb571c5cfe3e58d625972551b7211fc8058d
Dicky <dickyferdy5@gmail.com>
2023-12-11 08:44:24 +0700

    create collection & comment menu

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,11 +1,17 @@
 import { authUserSession } from "@/service/auth-service"
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
 
   return (
-    <div className="text-color-primary">
-      <h1>Welcome {user?.name}</h1>
+    <div className="text-color-primary flex flex-col mt-8 justify-center items-center gap-2">
+      <h5 className="text-2xl font-bold text-center">Welcome, {user?.name}</h5>
+      <img src={user?.image} alt="..." width={250} height={250}/>
+      <div className="flex flex-wrap py-8 gap-4">
+        <Link href="/users/dashboard/collection" className="bg-color-accent text-color-dark font-bold py-3 px-4">My Collection</Link>
+        <Link href="/users/dashboard/comment" className="bg-color-accent text-color-dark font-bold py-3 px-4">My Comment</Link>
+      </div>
     </div>
   );
 };
