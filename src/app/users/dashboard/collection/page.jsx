03680f13cd5d1cd7bfb00706a17180abb3319668
Dicky <dickyferdy5@gmail.com>
2023-12-13 10:02:52 +0700

    mapping collection from database

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,35 +1,30 @@
 import Header from "@/components/Dashboard/Header"
+import { authUserSession } from "@/service/auth-service";
+import prisma from "@/service/prisma";
 import Link from "next/link";
 
-const Page = () => {
+const Page = async () => {
+  const user = await authUserSession();
+  const collection = await prisma.collection.findMany({
+    where: {
+      user_email: user.email
+    }
+  });
+
   return (
     <section className="p-4">
       <Header title="My Collection"/>
       <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
-        <Link href="" className="relative border-2 border-color-accent">
-          <img src="" alt="" width={350} height={350} className="w-full max-h-80 object-cover"/>
-          <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="" className="relative border-2 border-color-accent">
-          <img src="" alt="" width={350} height={350} className="w-full max-h-80 object-cover"/>
-          <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="" className="relative border-2 border-color-accent">
-          <img src="" alt="" width={350} height={350} className="w-full max-h-80 object-cover"/>
-          <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="" className="relative border-2 border-color-accent">
-          <img src="" alt="" width={350} height={350} className="w-full max-h-80 object-cover"/>
-          <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
+        {collection?.map((anime, index) => {
+          return (
+            <Link href={`/anime/${anime.anime_mal_id}`} key={index} className="relative border-2 border-color-accent">
+              <img src={anime.anime_image} alt="" width={350} height={350} className="scalew-full max-h-80 object-cover"/>
+              <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+                <h5 className="md:text-xl text-center">{anime.anime_title}</h5>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </section>
   );
