d9aa6c47f4c2eaa162e4e72f8a0ec75b113503b5
Dicky <dickyferdy5@gmail.com>
2023-12-13 09:52:12 +0700

    fix back button

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { House } from "@phosphor-icons/react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+  const handleBack = (event) => {
+    event.preventDefault()
+    router.back()
+  }
+
   return (
     <div className="flex justify-center items-center h-96">
       <div className="flex justify-center items-center gap-2 flex-col">
         <House size={54} className="text-color-accent"/>
         <h3 className="text-color-primary text-2xl">Not Found</h3>
-        <Link href={'/'}>
-          <button className="bg-color-accent text-color-dark rounded p-2">Home</button>
-        </Link>
+        <button onClick={handleBack} className="bg-color-accent text-color-dark rounded py-2 px-5 font-bold text-lg">Back</button>
       </div>
     </div>
   );
